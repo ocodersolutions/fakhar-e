3b00a287d97103ee61f0b048e6ac0f550cf61a99
Quang Tran <topinfo93@gmail.com>
2016-08-17 13:35:02 +0700

    update category

--- module/Application/src/Application/Controller/ServiceController.php
+++ module/Application/src/Application/Controller/ServiceController.php
@@ -227,8 +227,57 @@ class ServiceController extends BaseActionController {
 
  
         $__viewVariables['defaultView'] = isset($aGetParams['dv']) && !empty($aGetParams['dv']) ? 'article' : 'outfit';
+        //category
+        $aGetParams = $this->params()->fromQuery();
+        
+        $oService = $this->getServiceLocator();
+        $oAttributes = $oService->get('ProductAttributesTable');
+        if( isset($aGetParams['mapped']) && $aGetParams['mapped']=='true' ) $sGetAttributesTreeFunction = 'getAttributesTreeForUIMapped';
+        else $sGetAttributesTreeFunction = 'getAttributesTreeForUI';
+        $aAttributes = $oAttributes->{$sGetAttributesTreeFunction}( isset($aGetParams['id']) && !empty($aGetParams['id']) ? $aGetParams['id'] : 1 );
+        //print_r($aAttributes); exit(0);
+        $iLevel = isset($aGetParams['levels']) && !empty($aGetParams['levels']) ? $aGetParams['levels'] : 4;
+        $aExcludeCats = isset($aGetParams['ec']) && !empty($aGetParams['ec']) ? explode(',', $aGetParams['ec']) : array();
+        $aJson = $this->printAttributes( $aAttributes, $iLevel, $aExcludeCats );
+        $__viewVariables['aJson'] = $aJson;
         
         return $__viewVariables;
+    }
+
+     private function printAttributes($aAttributes, $iLevel, $aExcludeCats)
+    {
+        $aFinal = array();
+        foreach( $aAttributes as $attLevel1 ) {
+//              if($attLevel1['depth'] > $iLevel) {
+//                  return array();
+//              }
+                $aTemp = array();                   
+            $aTemp['id'] = $attLevel1['id'];
+            $aTemp['label'] = $attLevel1['title'];
+            $aTemp['inode'] = (isset($attLevel1['child']) && is_array($attLevel1['child']) && count($attLevel1['child']));
+            $aTemp['open'] = false;
+            $aTemp['checkbox'] = true;
+            $aTemp['radio'] = false;
+            $aTemp['checked'] = false;
+            if( in_array($attLevel1['id'], $aExcludeCats) ) {
+                $aTemp['checked'] = false;
+            }
+            
+            if(@count($attLevel1['child'])) {
+                $aTemp['branch'] = $this-> printAttributes( $attLevel1['child'], $iLevel, $aExcludeCats );
+                
+                foreach( $aTemp['branch'] as $val ){
+                    if( $val['checked']===false ) {
+                        $aTemp['open'] = false;
+                        break;
+                    }
+                                        
+                }
+            }
+            
+            $aFinal[] = $aTemp;
+        }  
+        return $aFinal; 
     }
     
     public function shareOutfitAction() {
