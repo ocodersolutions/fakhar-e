d6a27cdd8b38e21898186ec5b8085825a41b7f2a
Hoang Trung <hoangtrungweb@gmail.com>
2016-09-01 10:44:32 +0700

    Fix some feedback from Fakhar!

--- queryupdate.sql
+++ queryupdate.sql
@@ -8,3 +8,6 @@ ALTER TABLE `articlealert`
   ADD PRIMARY KEY (`alertid`);
 ALTER TABLE `articlealert`
   MODIFY `alertid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
+
+
+ALTER TABLE `logs` ADD `searchVenue` VARCHAR(500) NOT NULL AFTER `searchArticle`;
