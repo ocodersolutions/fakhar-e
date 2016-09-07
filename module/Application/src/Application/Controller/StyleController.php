94d637bb636e03220458a733d644893e9f65d179
Hoang Trung <hoangtrungweb@gmail.com>
2016-09-07 15:00:19 +0700

    Build Style Controller

---
+++ module/Application/src/Application/Controller/StyleController.php
@@ -0,0 +1,33 @@
+<?php
+namespace Application\Controller;
+
+use Zend\View\Model\ViewModel;
+use Application\Model\FeedDataTable;
+use Zend\Session\Container;
+use Zend\Console\Request as ConsoleRequest;
+use Ocoder\Base\BaseActionController;
+
+class StyleController extends BaseActionController
+{
+	protected $_userid;
+	protected $_oService;
+    public function __construct(){
+    }
+
+	public function indexAction() 
+	{
+		$__viewVariables = array();
+		$this->layout('layout/layout_elnove.phtml');
+        $oAuth = $this->getServiceLocator()->get('AuthService');
+        if ($oAuth->hasIdentity() && $oAuth->getIdentity()->userType==2) {
+            $this->layout()->showHeaderLinks = "LOGGED_IN";
+            $userInfo = $oAuth->getIdentity();
+            $this->userId = $userInfo->userId;
+            $this->layout()->firstName = $userInfo->firstName;
+        } else {
+            $this->redirect()->toRoute('auth');
+        }
+
+		return 	$__viewVariables;
+	}
+}
