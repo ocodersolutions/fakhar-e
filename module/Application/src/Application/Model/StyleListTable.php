a8c01620763ec37cd1f51a0ecf0e797d8dc75480
Phan Văn Huy Hoàng <pvhuyhoang166@gmail.com>
2016-09-29 08:10:15 +0700

    update upgrade venue

--- module/Application/src/Application/Model/StyleListTable.php
+++ module/Application/src/Application/Model/StyleListTable.php
@@ -59,8 +59,7 @@ class StyleListTable extends BasicTableAdapter {
 		
         $sql = new Sql($this->getServiceLocator()->get('db'));
         $select = $sql->select(array('sl' => 'Style'));
-       	$select ->order(array('id DESC','id'));
-
+       	
         $resultSet = array();
         $results = $sql->prepareStatementForSqlObject($select)->execute();
         $resultSet = new \Zend\Db\ResultSet\ResultSet();
